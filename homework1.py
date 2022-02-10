a0b2c7bc3cf587a0c6d4a6b0ee43c2879d04c3d6
kirillkovalskiy <parikirill228@gmail.com>
2022-02-10 16:35:48 +0200

    homework1

---
+++ homework1.py
@@ -0,0 +1,18 @@
+# task 1
+name = "Kirill"
+day = "Thursday"
+print ("Good day" , name  ,'!', day ,"is a perfect day to learn some python." , end = '\n')
+# task 2
+first_name = 'kirill'
+last_name = 'kovalskyi'
+print (first_name.title(),last_name.title(),"hello!",sep = ' ')
+# task 3
+a = 5
+b = 2
+print ("a + b =",a+b,sep=' ')
+print ("a - b =",a-b,sep=' ')
+print ("a / b =",a/b,sep=' ')
+print ("a * b =",a*b,sep=' ')
+print ("a ^ b =",a**b,sep=' ')
+print ("a % b =",a%b,sep=' ')
+print ("a // b =",a//b,sep=' ')
