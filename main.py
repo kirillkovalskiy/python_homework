77eb18d49f84964d438568caf20f41b785e3ffe7
kirillkovalskiy <parikirill228@gmail.com>
2022-02-10 16:42:28 +0200

    homework1

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# task 1
+name = "Kirill"
+day = "Thursday"
+print ("Good day" , name  ,'!', day ,"is a perfect day to learn some python." , end = '\n')
+# task 2
+first_name = 'kirill'
+last_name = 'kovalskyi'
+print (first_name.title(),last_name.title(),"hello!",sep = ' ')
+# task 3
+a = 5
+b = 2
+print ("a + b =",a+b,sep=' ')
+print ("a - b =",a-b,sep=' ')
+print ("a / b =",a/b,sep=' ')
+print ("a * b =",a*b,sep=' ')
+print ("a ^ b =",a**b,sep=' ')
+print ("a % b =",a%b,sep=' ')
+print ("a // b =",a//b,sep=' ')
